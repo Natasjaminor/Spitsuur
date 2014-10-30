f202db9c5d50bcda75c142b8babc933ca3beed19
Rukshar <ruky_wh@hotmail.com>
2014-10-30 03:04:39 +0100

    matrix toegevoegd

--- rush.py
+++ rush.py
@@ -1,1 +1,20 @@
-# this is just a test for the python code
+
+# Tkinter zit standaard in python xy
+import Tkinter
+root = Tkinter.Tk()
+
+canvas = Tkinter.Canvas(root, width = 300, height = 300)
+canvas.pack()
+
+canvas.create_line(0,300,300,300)
+canvas.create_line(300,300,300,0)
+# nog geen linker verticale lijn en nog geen boven horizontale lijn
+
+for row in range(6): # maakt rijen en kolommen
+    for column in range(6):
+        canvas.create_line(50 *column, 0, 50 * column, 400)
+        canvas.create_line(0, 50 * row, 400, 50 * row)
+
+canvas.create_rectangle(150, 0, 100, 150, fill="purple") # Een auto
+
+root.mainloop()
