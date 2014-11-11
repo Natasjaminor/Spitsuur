43cd175bde1447a870ce439f811f17e80d33ec56
yasminakada <y.kada@live.nl>
2014-11-11 17:33:41 +0100

    added rushdfs.py, small changes in rush

--- rushdfs.py
+++ rushdfs.py
@@ -1,1 +1,10 @@
 import rush.py
+
+
+
+def BruteDfs(startboard,gamestates_set):
+	# for all cars check movability
+	# for every car try to move
+	# remember the gamestate in the set of gamestates
+	# check if the red car is on exit
+	# if not  go check next car.
